b1aab3dae53a9a5f0d96f15a9b41c25bb215fc76
mmertdogann <doganme@mef.edu.tr>
2020-09-04 23:48:55 +0300

    Application completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
-const alanKey =
-  'a4b2ba7eb477ee019671989263131c3e2e956eca572e1d8b807a3e2338fdd0dc/stage';
+import NewsCards from './components/NewsCards/NewsCards';
+
+import useStyles from './styles';
 
 const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+
+  const classes = useStyles();
+
   useEffect(() => {
     alanBtn({
-      key: alanKey,
-      onCommand: ({ command }) => {
-        if (command === 'testCommand') {
-          alert('This code was executed');
+      key:
+        'a4b2ba7eb477ee019671989263131c3e2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > 20) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
         }
       },
     });
@@ -18,7 +45,32 @@ const App = () => {
 
   return (
     <div>
-      <h1>Alan AI News Application</h1>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}>
+              <Typography variant='h5' component='h2'>
+                Try saying: <br />
+                <br />
+                Open article number [4]
+              </Typography>
+            </div>
+            <div className={classes.card}>
+              <Typography variant='h5' component='h2'>
+                Try saying: <br />
+                <br />
+                Go back
+              </Typography>
+            </div>
+          </div>
+        ) : null}
+        <img
+          src='https://alan.app/voice/images/previews/preview.jpg'
+          className={classes.alanLogo}
+          alt='logo'
+        />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
 };
---
+++ src/styles.js
@@ -0,0 +1,144 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  footer: {
+    textAlign: 'center',
+    position: 'fixed',
+    left: 0,
+    bottom: 0,
+    color: 'black',
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '120px',
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
+  link: {
+    textDecoration: 'none',
+    color: 'rgba(21, 101, 192)',
+  },
+  card: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '50%',
+    padding: '3%',
+    borderRadius: 10,
+    color: 'white',
+    backgroundColor: 'rgba(21, 101, 192)',
+    margin: '0 12px',
+    textAlign: 'center',
+    height: '25vmin',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column-reverse',
+      textAlign: 'center',
+      width: '100%',
+      height: 'initial',
+      '&:nth-of-type(1)': {
+        marginBottom: '12px',
+      },
+    },
+  },
+  infoContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+    },
+  },
+  logoContainer: {
+    padding: '0 5%',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '100%',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column-reverse',
+      textAlign: 'center',
+    },
+  },
+  alanLogo: {
+    height: '27vmin',
+    borderRadius: '15%',
+    padding: '0 5%',
+    margin: '3% 0',
+    [theme.breakpoints.down('sm')]: {
+      height: '35vmin',
+    },
+  },
+  particlesOptions: {
+    particles: {
+      number: {
+        value: 400,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: '#fff',
+      },
+      shape: {
+        type: 'circle',
+        stroke: {
+          width: 0,
+          color: '#ff0000',
+        },
+        polygon: {
+          nb_sides: 5,
+        },
+        image: {
+          src: '',
+          width: 100,
+          height: 100,
+        },
+      },
+      opacity: {
+        value: 1,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 2,
+          opacity_min: 0,
+          sync: false,
+        },
+      },
+      size: {
+        value: 20,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 20,
+          size_min: 0,
+          sync: false,
+        },
+      },
+      line_linked: {
+        enable: true,
+        distance: 100,
+        color: '#fff',
+        opacity: 1,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 2,
+        direction: 'none',
+        random: false,
+        straight: false,
+        out_mode: 'out',
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 3000,
+          rotateY: 3000,
+        },
+      },
+      array: [],
+    },
+  },
+}));
